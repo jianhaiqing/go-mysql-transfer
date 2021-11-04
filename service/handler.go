a0a57cc5b90257b8c408e97093cf0c0d33b99c64
jonathan <jianhaiqing@cvte.com>
2021-11-04 15:46:27 +0800

    replace github.com/siddontang/go-mysql with github.com/go-mysql-org/go-mysql

--- service/handler.go
+++ service/handler.go
@@ -22,10 +22,10 @@ import (
 	"log"
 	"time"
 
+	"github.com/go-mysql-org/go-mysql/canal"
+	"github.com/go-mysql-org/go-mysql/mysql"
+	"github.com/go-mysql-org/go-mysql/replication"
 	"github.com/juju/errors"
-	"github.com/siddontang/go-mysql/canal"
-	"github.com/siddontang/go-mysql/mysql"
-	"github.com/siddontang/go-mysql/replication"
 
 	"go-mysql-transfer/global"
 	"go-mysql-transfer/model"
